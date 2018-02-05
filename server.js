904a17e71ab81f0c56f711732f52e38a8d8bcf25
Frederiko Oliveira de Sousa <frederiko.sousa@globaltec.com.br>
2018-02-05 21:46:32 -0200

    changing files names

--- server.js
+++ server.js
@@ -51,7 +51,7 @@ app.get('/about', (req, res) => {
     });
 });
 
-app.get('/project', (req, res) => {
+app.get('/projects', (req, res) => {
     res.render('project.hbs', {
         pageTitle: 'Portifolio page',
         wellcomeMessage: 'My portifolio page'
